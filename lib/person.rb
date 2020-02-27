06f0c4f9a49b4fd67f938b0374d22af2484ccace
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-27 01:11:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,10 @@
 class Person
   #your code here
   
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,             :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh,             :weight, :handed, :complexion, :t_shirt_size, 
                 :wrist_size, :glove_size, :pant_length, :pant_width
+
+def initialize
+end
+
 end
